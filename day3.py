52dab02a251e8bc9de502157cef078c6ee3e5820
Denise <denise.eliza@gmail.com>
2021-12-03 10:29:08 -0500

    day 3

---
+++ day3.py
@@ -0,0 +1,77 @@
+filename = 'puzzle_input/day3.txt'
+my_input = []
+
+with open(filename) as file:
+    for line in file:
+        my_input.append(line.strip())
+
+def get_most_common(gmc_list, gmc_index):
+    # l is the input list
+    # i is which position we are looking at
+    
+    l_len = len(gmc_list)
+    gmc_counter = 0
+    
+    for this_l in gmc_list:
+        gmc_counter += int(this_l[gmc_index])
+    if gmc_counter >= l_len / 2:
+        return '1'
+    else:
+        return '0'
+        
+
+input_length = len(my_input)
+num_length = len(my_input[0])
+gamma = ""
+epsilon = ""
+x = 0
+for c in range(num_length):
+    most_common = get_most_common(my_input, c)
+    if most_common == '1':
+        gamma += '1'
+        epsilon += '0'
+    else:
+        epsilon += '1'
+        gamma += '0'
+    
+print("Part 1:", int(gamma, 2) * int(epsilon, 2))
+
+o2_list = []
+co2_list = []
+first_mc = get_most_common(my_input, 0)
+
+for c in range(input_length):
+    if my_input[c][0] == first_mc:
+        o2_list.append(my_input[c])
+    else:
+        co2_list.append(my_input[c])
+
+
+def reduce_list(this_list, switch):
+    # this_list is the list we are reducing
+    # switch is 0 or 1, to determine whether we want the most common or least common
+    
+    c = 1
+    while len(this_list) > 1:
+        mc = get_most_common(this_list, c)
+        if switch == 0:
+            if mc == '0':
+                mc = '1'
+            else:
+                mc = '0'
+                
+        list_len = len(this_list)
+        i = 0
+        while True:
+            try:
+                if this_list[i][c] == mc:
+                    i += 1
+                else:
+                    del this_list[i]
+            except IndexError:    # Stop at the end of the list
+                break
+        c += 1
+    return this_list[0]
+
+print("Part 2:", int(reduce_list(o2_list,1), 2) * int(reduce_list(co2_list,0), 2))
+
