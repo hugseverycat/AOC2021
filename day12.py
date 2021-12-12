4cd4a9ba7430376ea0cdaa3c68fff390e26f430d
Denise <denise.eliza@gmail.com>
2021-12-12 17:55:16 -0500

    day 12

--- day12.py
+++ day12.py
@@ -1,27 +1,36 @@
 from collections import defaultdict
 
 filename = 'puzzle_input/day12.txt'
-filename = 'puzzle_input/test_input.txt'
+#filename = 'puzzle_input/test_input.txt'
 
 cave_list = {}
 
+
+# This is an algorithm I shamelessly stole off stackoverflow and do not understand
 def all_simple_paths(caves, start, end, path):
     path = path + [start]
     if start == end:
         return [path]
     paths = []
     for child in caves[start]:
+        
+        # For part 2, I count how many of each lowercase cave we've visited
+        # If we've visited one twice, set twice to True
         twice = False
-        visited = list(map(lambda letter: {letter: path.count(letter)}, set(path)))
-        print(visited)
-        print(len(visited))
-        '''for v in visited:
-            if visited[v] > 1 and v.upper() != v:
-                twice = True'''
+        for p in path:
+            if p.upper() != p:
+                temp = path.count(p)
+                if temp == 2:
+                    twice = True
+                    break
+                    
+        # Switch the 2 ifs below to toggle between part 1 and part 2
         #if child not in path or child.upper() == child:
-        if child != 'start' and (child not in path or child.upper() == child):
+        if child not in path or child.upper() == child or not twice:
             child_paths = all_simple_paths(caves, child, end, path)
+            
             paths.extend(child_paths)
+            
     return paths
 
 with open(filename) as file:
@@ -37,14 +46,19 @@ with open(filename) as file:
         else:
             cave_list[b].append(a)
 
-'''for cave in cave_list:
-    print(cave, cave_list[cave])'''
-
-visits = defaultdict(int)
 all_paths = all_simple_paths(cave_list, 'start', 'end', [])
-'''for this_path in all_paths:
-    print(this_path)'''
 
-print(len(all_paths))
-#print(visits)
+
+# OK so my algorithm above allows us to visit 'start' twice and
+# i can't figure out how to fix it
+# but i can count how many paths have 'start' in it more than once
+# and delete them. so that's what i did.
+
+test_counter = 0
+for p in all_paths:
+    if p.count('start') > 1:
+        test_counter += 1
+
+print("Part 2:", len(all_paths) - test_counter)
+
     
