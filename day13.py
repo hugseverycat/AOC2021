edfa820b0129b98c07ce1005c75cf4c26d67e8e4
Denise <denise.eliza@gmail.com>
2021-12-13 11:19:23 -0500

    day 13, this time with sets

--- day13.py
+++ day13.py
@@ -1,7 +1,7 @@
 filename = 'puzzle_input/day13.txt'
 #filename = 'puzzle_input/test_input.txt'
 
-coords = {}
+coords = set()    # Storing as a set so we don't have to worry about duplicates
 folds = []
 raw_input = []
 max_x = 0
@@ -10,12 +10,12 @@ max_y = 0
 with open(filename) as file:
     for line in file:
         # When we hit the blank line, take all the input we've found
-        # so far and process it into the coords dictionary
+        # so far and process it into the coords set
         if line.strip() == '':
             for this_input in raw_input:
                 x, y = this_input.split(',')
                 x, y = int(x), int(y)
-                coords[(x, y)] = '#'
+                coords.add((x, y))
             # Clear the raw_input list; now we will store folds in it
             raw_input = []
         else:
@@ -29,31 +29,28 @@ for this_input in raw_input:
     folds.append((axis, value))
 
 
-def do_fold(coord_dict, fold):
+def do_fold(coord_set, fold):
     f_axis, f_value = fold
-    new_coord_dict = {}    # Storing these in a dictionary so we can
-                           # easily overwrite duplicates
+    new_coord_set = set()
     
-    for this_coord in coord_dict:
+    for this_coord in coord_set:
         cx, cy = this_coord
         if f_axis == 'x':
             # If cx is bigger than the fold, its x value will change
             # The formula for how it will change is 2 * fold - cx
             
-            # If cx is smaller, then it wont change, just add to new dict
+            # If cx is smaller, then it wont change, just add to new set
             if cx > f_value:
-                new_c = (2 * f_value - cx, cy)
-                new_coord_dict[new_c] = '#'
+                new_coord_set.add((2 * f_value - cx, cy))
             else:
-                new_coord_dict[(cx, cy)] = '#'
-        else: # f_axis == 'y'
+                new_coord_set.add((cx, cy))
+        else:  # f_axis == 'y'
             # Same as above, but for cy now.
             if cy > f_value:
-                new_c = (cx, 2 * f_value - cy)
-                new_coord_dict[new_c] = '#'
+                new_coord_set.add((cx, 2 * f_value - cy))
             else:
-                new_coord_dict[(cx, cy)] = '#'
-    return new_coord_dict
+                new_coord_set.add((cx, cy))
+    return new_coord_set
 
 for this_fold in folds:
     coords = do_fold(coords, this_fold)
